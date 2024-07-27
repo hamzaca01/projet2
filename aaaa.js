892cbff3e71bcb1cce3f95e1dc73ce94bdbd4123
hamzaca <ayedhamza970@gmail.com>
2024-07-27 13:06:05 +0200

    first commit

---
+++ aaaa.js
@@ -0,0 +1,8 @@
+const langs=["العربية","english","fransais"]
+console.log(langs);
+const redteam=["moez hassen","yassine bou3bid","hamza ben abda","med amine hamrouni","ghaith zaalouni","ahmed kalil" ,"new africain", "ghaith sgair","hamza khadraoui","bessem srarfi","kingsli edow"]
+//console.log(redteam);
+langs.push("italien");
+console.log(langs);
+langs.pop()
+console.log(langs);
